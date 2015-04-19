984dc870ea253d7a0710d55d87fad5f2a38deed8
Frank A. Krueger <fak@praeclarum.org>
2015-04-19 15:30:51 -0700

    Add HelloWorld sample

---
+++ HelloWorld/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NView.Controls;
+
+namespace HelloWorld
+{
+	public class HelloWorld
+	{
+		public readonly Label Label = new Label {
+			Text = "Hello, World!"
+		};
+	}
+}
+
---
+++ README.md
@@ -0,0 +1,6 @@
+# NView Samples
+
+## HelloWorld
+
+This is the most basic example with a single full screen label that says "Hello, World!".
+
